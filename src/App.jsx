1f47081c84964774b570d41695226bf31f3ca5c8
romnick <romnicksanjuan9@gmail.com>
2024-03-17 19:40:39 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import uwu from './image/uwu.jpg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>Sorry na Baby <span><img src={uwu} alt="" /></span>
+        
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
